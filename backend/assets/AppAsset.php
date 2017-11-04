6f038ea93ffa7ddb6d2641401eb3fe2d49808e4b
lijieaa <952054767@qq.com>
2017-11-04 21:47:28 +0800

    整合adminlte完成

--- backend/assets/AppAsset.php
+++ backend/assets/AppAsset.php
@@ -12,9 +12,18 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.css',
+       // 'css/site.css',
+        'adminlte/bootstrap/css/bootstrap.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css',
+        'adminlte/dist/css/AdminLTE.min.css',
+        'adminlte/dist/css/skins/skin-blue.min.css',
+
     ];
     public $js = [
+        'adminlte/plugins/jQuery/jquery-2.2.3.min.js',
+        'adminlte/bootstrap/js/bootstrap.min.js',
+        'adminlte/dist/js/app.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
